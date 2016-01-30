196472fd9f19ca6f8a84ce5259320dafaf6e9ef8
kevguy <kevlai@uw.edu>
2016-01-31 05:09:38 +0800

    Edit code to make it up to heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
 
-var agenda = require('agenda')({ db: { address: 'localhost:27017/test' } });
+var agenda = require('agenda')({ db: { address: 'mongodb://kevlai22:forever1993@ds053295.mongolab.com:53295/kev' } });
 var sugar = require('sugar');
 var nodemailer = require('nodemailer');
 var _ = require('lodash');
@@ -82,7 +82,7 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
 var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
 
-mongoose.connect('localhost');
+mongoose.connect('mongodb://kevlai22:forever1993@ds053295.mongolab.com:53295/kev');
 
 var app = express();
 
