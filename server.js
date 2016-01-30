11b14d4913252fb0ceb13b1fbc9f387d7baf1ef1
kevguy <kevlai22@uw.edu>
2016-01-31 03:10:31 +0800

    initial commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var async = require('async');
 var request = require('request');
 var xml2js = require('xml2js');
 
-var agenda = require('agenda')({ db: { address: 'localhost:27017/test' } });
+var agenda = require('agenda')({ db: { address: 'mongodb://kevlai22:forever1993@ds053295.mongolab.com:53295/kev' } });
 var sugar = require('sugar');
 var nodemailer = require('nodemailer');
 var _ = require('lodash');
